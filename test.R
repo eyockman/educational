c6c5dab269ec7c909072f82ed7b5f36ed96694fa
Emma Yockman <eyockman@ucsc.edu>
2025-04-08 22:14:04 -0700

    syntax fixes

--- test.R
+++ test.R
@@ -1,21 +1,21 @@
 rm(list=ls())
 library(ggplot2)
 
-data <- read.csv("~/Documents/Coding/hester/tutorial/test.csv")
-head(data)
+frog = read.csv("~/Documents/Coding/educational/test.csv")
+head(frog)
 
-ggplot(data, aes(x=species, y=height, fill=species)) +
+ggplot(frog, aes(x=species, y=height, color=species)) +
   geom_boxplot() +
   theme_bw()
 
 # compare heights of these three species (using ANOVA)
-height_comparison <- aov(height~species, data=data)
+height_comparison=aov(height~species, data=data)
 summary(height_comparison) # p = 0.0273 , the species are significantly different!
 
 # post hoc test to determine which ones are different (we'll use Tukey's for now)
 TukeyHSD(height_comparison)
 
-#output
+# output
 # $species
 #                     diff       lwr      upr     p adj
 # raphanus-clarkia    -5.4 -23.92453 13.12453 0.7231981
@@ -23,3 +23,6 @@ TukeyHSD(height_comparison)
 # trifolium-raphanus -15.6 -34.12453  2.92453 0.1032118
 
 # tells us that trifolium and clarkia are significantly different, but not any of the other comparisions. This is validated by our boxplot!
+# Comparison of means showed that species were significantly different (F= 4.9, df=2, p=0.03). 
+# Tukey's Post Hoc revealed that ....
+# The mean heights of trifolium and clarkia were significantly different (F, df, p)
