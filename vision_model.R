20e1392c1bdabc9f31505fad7554e8a042e01abb
Emma Yockman <eyockman@ucsc.edu>
2025-04-18 18:10:16 -0700

    added better annotations

--- vision_model.R
+++ vision_model.R
@@ -7,26 +7,27 @@ library(ggsurvfit)
 library(survminer)
 library(car)
 
-data("retinopathy", package="survival")
+data("retinopathy", package="survival") # load in dataset that comes with the survival package
 
-head(retinopathy)
+head(retinopathy) # look at first few rows
 
 # plot!
-fit <- survfit(Surv(futime,status) ~ trt, data=retinopathy)
-ggsurvplot(fit, data=retinopathy, 
-           size = 1,
-           palette = c("#ABC864","#795F9E"),
-           conf.int = TRUE,
-           legend.labs = c("Control Eye","Treated Eye"),
-           xlab = "Time (days)",
-           ylab = "Survival Probability",
-           ggtheme = theme_bw())
+fit <- survfit(Surv(futime,status) ~ trt, data=retinopathy) # make a km survival curve that separates by treatment (trt)
+ggsurvplot(fit, data=retinopathy, # plot the "fit" object using retinopathy data; using ggsurvplot (ggplot won't work with a survfit object)
+           size = 1, # size of lines
+           palette = c("#ABC864","#795F9E"), # manually set color palette
+           conf.int = TRUE, # put confidence intervals on plot
+           legend.labs = c("Control Eye","Treated Eye"), # label the legend based on the treatments
+           xlab = "Time (days)", # label x axis
+           ylab = "Survival Probability", # label y axis
+           ggtheme = theme_bw()) # set theme
 
 # model
-model <- coxph(Surv(futime, status) ~ trt, data = retinopathy)
+# the plot used a Kaplan Meier curve, but we want to do cox proportional hazards, so it's a different function
+model <- coxph(Surv(futime, status) ~ trt, data = retinopathy) # make a cox model looking at time to event, status (of event), impacted by treatment in the retinopathy dataset
 
-#is our effect significant?
-Anova(model, test="LR") # yes!
+#is our effect (treatment) significant?
+Anova(model, test="LR") # yes! p = 2.246e-06 ***
 
 # interpret the model
 print(model) # hazard ratio = 0.46 -> since it's less than 1, that means it DECREASES the risk of event
