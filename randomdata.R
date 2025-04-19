04302a9d70df941608f3b83fbd76e9d823289c27
Emma Yockman <eyockman@ucsc.edu>
2025-04-18 17:51:44 -0700

    annotated code, updated a few things

--- randomdata.R
+++ randomdata.R
@@ -1,9 +1,12 @@
-rm(list=ls())
+rm(list=ls()) # clear R's brain
 
-water=rnorm(100, 0, 0.5)
-grubs=water*rnorm(100, 5, 3)+20
-t=data.frame(water, grubs) # randomly generated data set
+water=rnorm(100, 0, 0.5) # randomly generate a list of 100 numbers with an average of 0 and a standard deviation of 0.5. Call it "water"
+grubs=water*rnorm(100, 5, 3)+20 # randomly generate a list of 100 numbers with an average of 5, std dev of 3, but multiply every number by water and add 20
+t=data.frame(water, grubs) # randomly generated data set; put water and grubs together into a data frame
 
-ggplot(t, aes(x=water, y=grubs))+
-  geom_point(pch=1, col="blue", size=3)+
-  theme_few()+labs(x="",y="")
+ggplot(data=t, aes(x=water, y=grubs))+ # make a plot using t, with water as x axis and grubs as y
+  geom_point(pch=1, # draw points on the plot using my data t mentioned in the last line, but draw points using pch style 1
+             col="#4EA699", # make points this color
+             size=2)+  # make points this size
+  theme_few()+ # use a cuter theme
+  labs(x="change in water level (mL)",y="grubs are creatures") # manually label the x and y axis
