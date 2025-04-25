b57d063f147ea38778ddca4e3222d6f85ec5e16b
Emma Yockman <eyockman@ucsc.edu>
2025-04-24 19:47:27 -0700

    load package

--- randomdata.R
+++ randomdata.R
@@ -1,8 +1,9 @@
 rm(list=ls()) # clear R's brain
+library(ggplot2) # load in ggplot2 package
 
 water=rnorm(100, 0, 0.5) # randomly generate a list of 100 numbers with an average of 0 and a standard deviation of 0.5. Call it "water"
 grubs=water*rnorm(100, 5, 3)+20 # randomly generate a list of 100 numbers with an average of 5, std dev of 3, but multiply every number by water and add 20
-t=data.frame(water, grubs) # randomly generated data set; put water and grubs together into a data frame
+t=data.frame(water, grubs) # randomly generated data set; put water and grubs together into a data frame (data.frame() is a base r function)
 
 ggplot(data=t, aes(x=water, y=grubs))+ # make a plot using t, with water as x axis and grubs as y
   geom_point(pch=1, # draw points on the plot using my data t mentioned in the last line, but draw points using pch style 1
